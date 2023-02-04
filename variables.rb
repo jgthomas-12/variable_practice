5500958bc880d59a63bb14e8acae0e2636d18a99
Jesse Thomas <jgthomas3@gmail.com>
2023-02-03 17:59:03 -0700

    Add 4 integer variables

--- variables.rb
+++ variables.rb
@@ -1,4 +1,13 @@
+## Strings
+
 name = "Jesse"
 last_name = "Thomas"
 pseudonym = "Donkey"
 be_nice = "Don't refer to yourself as Donkey, donkey"
+
+## Integers
+
+year = 2023
+meaning_of_life = 42
+bone_creature_members = 4
+great_lakes = 5
